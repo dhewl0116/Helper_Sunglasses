1ea21bc350daed82a0120e9cc6b40c49bdc2404e
Team-Always <152771549+Team-Always@users.noreply.github.com>
2023-12-03 17:59:39 +0900

    Delete Always_Guardian directory

--- Always_Guardian/mic_info.py
+++
@@ -1,9 +0,0 @@
-import pyaudio
-
-
-PA = pyaudio.PyAudio()
-
-for device_index in range(PA.get_device_count()):
-    info = PA.get_device_info_by_index(device_index)
-    for key in info.keys():
-        print(key, ' = ', info[key])
